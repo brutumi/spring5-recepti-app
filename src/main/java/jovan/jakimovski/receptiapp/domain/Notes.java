b224297b0e66e94ea9f1d7672a26e99b48e3193e
brutumi <jovanj9730@gmail.com>
2019-01-13 22:32:17 +0100

    added couple model classes

---
+++ src/main/java/jovan/jakimovski/receptiapp/domain/Notes.java
@@ -0,0 +1,40 @@
+package jovan.jakimovski.receptiapp.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Notes {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne               //Ne sakame Cascade opcii TUKA, ako go izbrisam Notes NE SAKAM DA JA IZBRISE I RECEPT
+    private Recipe recipe;
+
+    @Lob                    //Large Object -> za da cuvame poveke od default 255
+    private String recipeNotes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getRecipeNotes() {
+        return recipeNotes;
+    }
+
+    public void setRecipeNotes(String recipeNotes) {
+        this.recipeNotes = recipeNotes;
+    }
+}
